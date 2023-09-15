09a37ef9be08dfed58a254a6a85727e2d35f0e23
Narayana Raju <narayanaraju060@gmail.com>
2023-09-15 10:51:22 +0530

    test

--- fantasygame/urls.py
+++ fantasygame/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('config_panel.urls'))
+    path('', include('config_panel.urls')),
 ]
